bb598e41f901aac35c5f3f3f2ecc07fc8e94cb3d
realStrixxy <47069036+realStrixxy@users.noreply.github.com>
2022-08-25 19:26:39 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,66 +5,39 @@ function randomString(length, chars) {
 }
 
 function GenerateNewPass() {
-    if(document.getElementById("noLength") !== null){
-        let newPDiv = document.getElementById("NewPasswordDiv");
-        let elem = document.querySelector("#noLength");
-        newPDiv.removeChild(elem);
-    }
-    if(document.getElementById("newP") !== null){
-        let newPDiv = document.getElementById("NewPasswordDiv");
-        let elem = document.querySelector("#newP");
-        newPDiv.removeChild(elem);
-    }
-
     let Length = document.getElementById("Plen").value;
     let Alphanumerical = document.getElementById("Alphanum");
     let Symbols = document.getElementById("Symbols");
+    let showNewPass = document.getElementById("newPassword");
 
     if(Length > 16) {
         Length = 16;
     }
 
-    if(Length == 0) {
-        let tag = document.createElement("p");
-        tag.append("No Password Length");
-        tag.setAttribute("id", "noLength");
-        let element = document.getElementById("NewPasswordDiv");
-        element.appendChild(tag);
-    }
-
     if(!Alphanumerical.checked && !Symbols.checked) {
-        let tag = document.createElement("p");
         let text = randomString(Length, '0123456789');
-        tag.append(text);
-        tag.setAttribute("id", "newP");
-        let element = document.getElementById("NewPasswordDiv");
-        element.appendChild(tag);
+        showNewPass.value = text;
     }
 
     if(Alphanumerical.checked && !Symbols.checked) {
-        let tag = document.createElement("p");
         let text = randomString(Length, '0123456789abcdekpqrstuvwxyzFGHIJKLMNOPQRSTYZ');
-        tag.append(text);
-        tag.setAttribute("id", "newP");
-        let element = document.getElementById("NewPasswordDiv");
-        element.appendChild(tag);
+        showNewPass.value = text;
     }
 
     if(!Alphanumerical.checked && Symbols.checked) {
-        let tag = document.createElement("p");
         let text = randomString(Length, '0123456789!$&%#*');
-        tag.append(text);
-        tag.setAttribute("id", "newP");
-        let element = document.getElementById("NewPasswordDiv");
-        element.appendChild(tag);
+        showNewPass.value = text;
     }
 
     if(Alphanumerical.checked && Symbols.checked) {
-        let tag = document.createElement("p");
         let text = randomString(Length, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$&');
-        tag.append(text);
-        tag.setAttribute("id", "newP");
-        let element = document.getElementById("NewPasswordDiv");
-        element.appendChild(tag);
+        showNewPass.value = text;
     }
+}
+
+function CopyNewPass() {
+    let showNewPass = document.getElementById("newPassword");
+    showNewPass.focus();
+    showNewPass.select();
+    document.execCommand('copy');
 }
